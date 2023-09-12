0dad805576bedb65c9662d10c061513707af680e
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-12 03:24:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            string name;
+            int age;
+            string LastName = "Mncube";
+
+            //initialization
+            name = "Nokubongwa";
+            age = 26;
+
+            //pririnting console
+            Console.WriteLine("Hi i am " + name + " " + LastName);
+            Console.WriteLine("I am " + age);
+
+            //changing the value of varaiables
+            name = "Ntombizodwa";
+            age = 30;
+
+            Console.WriteLine("I never liked my name, at age  " + age + " i changed it to " + name + " " + LastName);
+
+
+                
+
+
+            //freeze console
+            Console.ReadLine();
+
+
+
+
+
+
+
+                
+
+        }
+    }
+}
